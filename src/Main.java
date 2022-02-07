b9b0ca3ee8d6ee3e4c2751ca5403de6caa37f2d9
Ксения Алтухова <bogomolowakseniya@yandex.ru>
2022-02-07 22:08:28 +0300

    add new comments

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int account = 400; // сумма на счете
+        int account = 400; // текущий счет
         int refill = 5000; // сумма пополнения
 
         int bonus; // бонус с пополнения
-        int amount; // итоговая сумма на счете
+        int amount; // итоговый счет
         if (refill > 1000) {
             bonus = (refill / 100);
             amount = (refill + bonus + account);
